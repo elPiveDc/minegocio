9d08517c0ea51298a17d590fc923d98e6431200a
elPiveDc <Mateo23Dc@gmail.com>
2025-08-18 18:38:43 -0500

    Se agrego Nav y Footer

--- src/main/java/com/minegocio/backend/controller/HomeController.java
+++ src/main/java/com/minegocio/backend/controller/HomeController.java
@@ -5,11 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    
 
     @GetMapping("/")
     public String index() {
-        
+
         return "index";
     }
 
@@ -27,4 +26,5 @@ public class HomeController {
     public String dashboard() {
         return "dashboard";
     }
+
 }
