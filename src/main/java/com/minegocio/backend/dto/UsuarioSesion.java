d2fd44f9206544ec6ba7851543ed8423c316d780
elPiveDc <Mateo23Dc@gmail.com>
2025-10-24 19:03:23 -0500

    no

--- src/main/java/com/minegocio/backend/dto/UsuarioSesion.java
+++ src/main/java/com/minegocio/backend/dto/UsuarioSesion.java
@@ -1,12 +1,10 @@
 package com.minegocio.backend.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+@Getter
+@Setter
 public class UsuarioSesion {
     private Integer idUsuario;
     private String nombre;
@@ -14,4 +12,15 @@ public class UsuarioSesion {
     private boolean esAdmin;
     private String franquicia;
     private ConexionBdProjection conexion;
+
+    public UsuarioSesion(Integer idUsuario, String nombre, String correo, boolean esAdmin,
+            ConexionBdProjection conexion, String franquicia) {
+        this.idUsuario = idUsuario;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.esAdmin = esAdmin;
+        this.conexion = conexion;
+        this.franquicia = franquicia;
+    }
+
 }
